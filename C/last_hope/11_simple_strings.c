53ad19eda545c9baec2d5130cfb27eaea33fbf3a
neimv <neimv.zatara@gmail.com>
2023-02-09 18:54:38 -0600

    ejecrcicon de C

---
+++ C/last_hope/11_simple_strings.c
@@ -0,0 +1,8 @@
+/**
+ * @author      : neimv (neimv@dark-universe)
+ * @file        : 08_arrays_2d
+ * @created     : domingo feb 05, 2023 14:09:51 CST
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
