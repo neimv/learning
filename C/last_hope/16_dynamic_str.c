e1d021293f7e7a0768f296c33e5741e596c54983
neimv <neimv.zatara@gmail.com>
2023-02-20 18:09:41 -0600

    in c course

---
+++ C/last_hope/16_dynamic_str.c
@@ -0,0 +1,25 @@
+/**
+ * @author      : neimv (neimv@dark-universe)
+ * @file        : 16_dynamic_str
+ * @created     : lunes feb 20, 2023 18:01:44 CST
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main()
+{
+    char c;
+    char *str = (char*) calloc(1, sizeof(char));
+
+    printf("The size of char is: %ld\n", sizeof(c));
+    printf("The pointer of string is: %p\n", &str);
+    printf("The value of string is: %s\n", str);
+
+    while ()
+
+    return EXIT_SUCCESS;
+}
+
